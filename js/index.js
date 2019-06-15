493267be8d06c25d59032758f845a42297e0788d
lnx1337 <joshua13375@gmail.com>
2019-06-15 01:55:04 -0500

    Actualización de estilos

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".title-products").fadeIn(1200);
+    $(".title-products").fadeIn(1300);
     $(".fadeIn-one").fadeIn(4000);
     $(".fadeIn-two").fadeIn(5000);
     $(".fadeIn-three").fadeIn(6000);
