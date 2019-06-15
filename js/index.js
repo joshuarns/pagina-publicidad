cd7cd9db59eb85f095b0530859c81487b9df1d08
lnx1337 <joshua13375@gmail.com>
2019-06-15 01:22:15 -0500

    Corrección de errores en menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    $(".title-products").fadeIn(3000);
+    $(".title-products").fadeIn(1200);
+    $(".fadeIn-one").fadeIn(4000);
+    $(".fadeIn-two").fadeIn(5000);
+    $(".fadeIn-three").fadeIn(6000);
 });
