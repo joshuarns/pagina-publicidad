4c34e0854a7ffccccf734d6510dad71a6c029dbf
lnx1337 <joshua13375@gmail.com>
2019-06-15 21:29:17 -0500

    Se crean efectos en imagenes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-    $(".title-products").fadeIn(1300);
+    $('.title-products').animate(
+        {
+            left : "+=30",
+            opacity : 79
+        },
+        900
+      );
     $(".fadeIn-one").fadeIn(4000);
     $(".fadeIn-two").fadeIn(5000);
     $(".fadeIn-three").fadeIn(6000);
